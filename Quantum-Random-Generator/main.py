79bbb1e74ae536459e35c05bcafadd0a00090e69
Issac Manova M <149663619+IssacManova@users.noreply.github.com>
2026-02-13 21:51:00 +0530

    Update main.py

--- Quantum-Random-Generator/main.py
+++ Quantum-Random-Generator/main.py
@@ -2,27 +2,20 @@ from qiskit import QuantumCircuit, transpile
 from qiskit_aer import AerSimulator
 from qiskit.visualization import plot_histogram
 import matplotlib.pyplot as plt
-
 print("Program started...")
-
 def generate_random_bits(shots=1024):
     qc = QuantumCircuit(1, 1)
     qc.h(0)
     qc.measure(0, 0)
-
     simulator = AerSimulator()
     compiled = transpile(qc, simulator)
     result = simulator.run(compiled, shots=shots).result()
     counts = result.get_counts()
-
     return counts
-
 if __name__ == "__main__":
     shots = 1000
     counts = generate_random_bits(shots)
-
     print("Quantum Random Output:")
     print(counts)
-
     plot_histogram(counts)
     plt.show()
